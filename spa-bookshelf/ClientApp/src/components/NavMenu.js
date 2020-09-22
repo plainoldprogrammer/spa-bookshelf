ff6626068457192be020cb91e4e78429af5c8f47
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-09-22 18:52:54 -0500

    modifying the items on the navbar

--- spa-bookshelf/ClientApp/src/components/NavMenu.js
+++ spa-bookshelf/ClientApp/src/components/NavMenu.js
@@ -31,13 +31,13 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Books</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Authors</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Editorials</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
