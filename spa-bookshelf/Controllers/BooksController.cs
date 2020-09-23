5267111ba75b5463f0a52ec46b823dedf909341c
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-09-22 21:27:56 -0500

    fixing the indentation

--- spa-bookshelf/Controllers/BooksController.cs
+++ spa-bookshelf/Controllers/BooksController.cs
@@ -1,43 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace spa_bookshelf.Controllers
 {
-	[ApiController]
-	[Route("[Controller]")]
-	public class BooksController : ControllerBase
-	{
-		private readonly ILogger<BooksController> _logger;
+    [ApiController]
+    [Route("[Controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
 
-		public BooksController(ILogger<BooksController> logger)
-		{
-			_logger = logger;
-		}
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
 
-		[HttpGet]
-		public IEnumerable<Book> Get()
-		{
-			Console.WriteLine("Getting books data from Get()");
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            Console.WriteLine("Getting books data from Get()");
 
-			List<Book> theBooks = new List<Book>();
+            List<Book> theBooks = new List<Book>();
 
-			Book firstBook = new Book 
-			{
-				Title = "The C Programming Language"
-			};
+            Book firstBook = new Book
+            {
+                Title = "The C Programming Language"
+            };
 
-			Book secondBook = new Book	
-			{
-				Title = "Head First Design Patterns"
-			};
+            Book secondBook = new Book
+            {
+                Title = "Head First Design Patterns"
+            };
 
-			theBooks.Add(firstBook);
-			theBooks.Add(secondBook);
+            theBooks.Add(firstBook);
+            theBooks.Add(secondBook);
 
-			return theBooks.ToArray();
-		}
-	}
+            return theBooks.ToArray();
+        }
+    }
 }
