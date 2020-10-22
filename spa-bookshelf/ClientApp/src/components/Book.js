c147304f31a52a07e0dc909f7920f45e0fc330a8
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-10-21 21:29:36 -0500

    renaming the home component to book

--- spa-bookshelf/ClientApp/src/components/Book.js
+++ spa-bookshelf/ClientApp/src/components/Book.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export class Book extends Component {
+  static displayName = Book.name;
 
   render () {
     return (
